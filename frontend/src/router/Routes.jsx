b91ffe02c07d0f90b17e00befa66d6f9a4b7d15a
gisa001 <lenmicr250@gmail.com>
2024-03-12 14:55:40 +0530

    created feedbackmodel

--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
-import Project from '../pages/Project'
+import Project from '../pages/project'
 import Header from '../components/Header'
 
 export default function PageRoutes() {
