c09883e2f2d9372a10f48f51a3df213eb1a24010
Thisara <sasmithajayasinghe1@gmail.com>
2024-03-13 15:48:30 +0530

    jj

--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
-import Project from '../pages/project'
-import Header from '../components/Header'
+// import Header from '../components/Header'
 import AdminLayout from '../Admin/AdminLayout'
 
 export default function PageRoutes() {
@@ -9,7 +8,7 @@ export default function PageRoutes() {
     <>
     {/* <Header/> */}
     <Routes>
-      <Route path='/projects' element={<Project/>}/>
+      {/* <Route path='/projects' element={<Project/>}/> */}
       <Route path='/admin/hi' element={<AdminLayout/>}/>
     </Routes>
     </>
