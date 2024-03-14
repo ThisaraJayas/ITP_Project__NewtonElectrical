37013fabe22dbd458ec600ca2ceea16409bf71ac
Thisara <sasmithajayasinghe1@gmail.com>
2024-03-14 09:53:15 +0530

    created user context

--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -5,16 +5,19 @@ import AdminHome from '../Admin/AdminHome'
 import Project from '../pages/Project'
 import Product from '../Admin/components/Product'
 import UserManager from '../Admin/UserManager'
+import UseContextProvider from '../context/UserContext'
 
 export default function PageRoutes() {
   return (
     <>
+    <UseContextProvider>
     <Routes>
       <Route path='/projects' element={<Project/>}/>
       <Route path='/admin' element={<AdminHome/>}/>
       <Route path='/admin/product' element={<Product/>}/>
       <Route path='/admin/user' element={<UserManager/>}/>
     </Routes>
+    </UseContextProvider>
     </>
   )
 }
