5e71da19998bb7f02ef50841dc86b21d69e0d711
ThisaraJ <124505409+ThisaraJayas@users.noreply.github.com>
2024-03-15 21:31:25 +0530

    Revert grf

--- backend/routes/ProjectRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { InsertProject } from '../controllers/ProjectController.js'
-
-const router = express.Router()
-
-router.post('/projects',InsertProject)
-
-export default router
