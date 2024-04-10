857c78c061d2c96f9794e306a26e16403b45ebc1
Thisara <sasmithajayasinghe1@gmail.com>
2024-04-11 00:06:24 +0530

    store

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -1,13 +1,85 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import "../styles/store.css";
 
 export default function Store() {
   return (
     <div>
-      <Header/>
-        <div className='pt-20'>
-            sggrgr
+      <Header />
+      <div className="pt-20 flex justify-between">
+        <div className="">
+          gfgfdhf
         </div>
+        <div className="cardContents ml-9 mr-9">
+        <div class="grid grid-cols-3 gap-10">
+          <div class="item-card">
+            <div class="item-image">
+              <img src="https://via.placeholder.com/150" alt="Product Image" />
+            </div>
+            <div class="item-details">
+              <h2 class="itemName">Product Name</h2>
+              <div class="item-price">LKR 19.99</div>
+              <div className="btnSection flex justify mt-4">
+                <button class="addToCartBtn">Add to Cart</button>
+                <button class="buyNowBtn  ml-3">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="item-card">
+            <div class="item-image">
+              <img src="https://via.placeholder.com/150" alt="Product Image" />
+            </div>
+            <div class="item-details">
+              <h2 class="itemName">Product Name</h2>
+              <div class="item-price">LKR 19.99</div>
+              <div className="btnSection flex justify mt-4">
+                <button class="addToCartBtn">Add to Cart</button>
+                <button class="buyNowBtn  ml-3">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="item-card">
+            <div class="item-image">
+              <img src="https://via.placeholder.com/150" alt="Product Image" />
+            </div>
+            <div class="item-details">
+              <h2 class="itemName">Product Name</h2>
+              <div class="item-price">LKR 19.99</div>
+              <div className="btnSection flex justify mt-4">
+                <button class="addToCartBtn">Add to Cart</button>
+                <button class="buyNowBtn  ml-3">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="item-card">
+            <div class="item-image">
+              <img src="https://via.placeholder.com/150" alt="Product Image" />
+            </div>
+            <div class="item-details">
+              <h2 class="itemName">Product Name</h2>
+              <div class="item-price">LKR 19.99</div>
+              <div className="btnSection flex justify mt-4">
+                <button class="addToCartBtn">Add to Cart</button>
+                <button class="buyNowBtn  ml-3">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="item-card">
+            <div class="item-image">
+              <img src="https://via.placeholder.com/150" alt="Product Image" />
+            </div>
+            <div class="item-details">
+              <h2 class="itemName">Product Name</h2>
+              <div class="item-price">LKR 19.99</div>
+              <div className="btnSection flex justify mt-4">
+                <button class="addToCartBtn">Add to Cart</button>
+                <button class="buyNowBtn  ml-3">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
