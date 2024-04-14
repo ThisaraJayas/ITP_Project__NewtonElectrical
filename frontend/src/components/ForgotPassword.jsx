7dc51e741ea4054b895fc9088a47c0d1c68a5386
Thisara <sasmithajayasinghe1@gmail.com>
2024-04-14 14:44:25 +0530

    password reset

--- frontend/src/components/ForgotPassword.jsx
+++ frontend/src/components/ForgotPassword.jsx
@@ -1,34 +1,50 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../styles/forgotpassword.css";
+import Paper from '@mui/material/Paper';
+
+
 
 export default function ForgotPassword() {
-    const [email,setEmail] = useState('')
-    const navigate = useNavigate()
-    console.log(email);
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  console.log(email);
 
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
-        try {
-            const response = await axios.post('http://localhost:3000/auth/forgotpassword', {
-              email,
-            });
-            
-            if (response.status === 200) {
-              navigate('/login');
-            }
-          }catch(error){
-            console.log(error);
-          }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/auth/forgotpassword",
+        {
+          email,
+        }
+      );
+
+      if (response.status === 200) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
   return (
-    <div>
+    <div className="forgotpassContainer">
+      <Paper sx={{ width: '50%', maxWidth: 'none', boxShadow: 16 }} className='forgotpasswordPaper'>
+        <h2>Forgot your Password?</h2>
+        <p>Enter your email address below and we’ll send you a link to reset your password</p>
         <form onSubmit={handleSubmit}>
-
-            <input type='text' value={email} onChange={(e)=>setEmail(e.target.value)}  />
-            <input type='submit' value='submit'/>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            className="emailInput"
+          />
+          <input type="submit" value="Submit" className="submitButton" />
         </form>
+      </Paper>
     </div>
-  )
+  );
 }
--- frontend/src/components/ResetPassword.jsx
+++ frontend/src/components/ResetPassword.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import "../styles/resetpassword.css";
+
 
 
 export default function ResetPassword() {
     const [password,setPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
     const navigate = useNavigate()
     const {id,token}=useParams()
     console.log(password);
 
     const handleSubmit = async(e)=>{
         e.preventDefault()
-        try {
+        if(password===newPassword){
+          try {
             const response = await axios.post(`http://localhost:3000/auth/resetPassword/${id}/${token}`, {
                 password,
             });
@@ -22,15 +27,32 @@ export default function ResetPassword() {
           }catch(error){
             console.log(error);
           }
+        }else{
+          console.log('Password doent match');
+        }
+       
     }
 
   return (
-    <div>
+    <div className="forgotpassContainer">
+      <Paper sx={{ width: '50%', maxWidth: 'none', boxShadow: 16 }} className='forgotpasswordPaper'>
+        <h2>Enter New Password</h2>
         <form onSubmit={handleSubmit}>
-
-            <input type='text' value={password} onChange={(e)=>setPassword(e.target.value)}  />
-            <input type='submit' value='update'/>
+          <input
+            type="text"
+            value={password} onChange={(e)=>setPassword(e.target.value)}
+            placeholder="Enter your new password"
+            className="passwordInput"
+          />
+          <input
+            type="text"
+            value={newPassword} onChange={(e)=>setNewPassword(e.target.value)}
+            placeholder="Confirm your new password"
+            className="passwordInput"
+          />
+          <input type="submit" value="Update" className="submitButton" />
         </form>
+      </Paper>
     </div>
   )
 }
