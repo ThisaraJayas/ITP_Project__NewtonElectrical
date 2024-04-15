f1e2daf5915cdc262d25c7f2bf0047fcd98a2a8d
pemindi <pemindiw@gmail.com>
2024-04-15 16:16:40 +0530

    job

--- frontend/src/Admin/components/JobUpdate.jsx
+++ frontend/src/Admin/components/JobUpdate.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-
-
+import jobImage from '../images/job1.png';
 
 const JobUpdate = () => {
   const [title, setTitle] = useState('');
@@ -15,7 +14,6 @@ const JobUpdate = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
-  
 
   useEffect(() => {
     setLoading(true);
@@ -32,10 +30,10 @@ const JobUpdate = () => {
         setLoading(false);
       }).catch((error) => {
         setLoading(false);
-        alert('An error happened. Please Chack console');
+        alert('An error happened. Please check console');
         console.error('An error happened:', error);
       });
-  }, [id]); // Make sure to include id as a dependency
+  }, [id]); 
 
   const handleUpdate = () => {
     const updatedJob = {
@@ -52,7 +50,7 @@ const JobUpdate = () => {
       .then(() => {
         setLoading(false);
         console.log('Job updated successfully');
-        navigate('/JobsTable'); // Redirect to home page after successful update
+        navigate('/JobsTable'); 
       }).catch((error) => {
         setLoading(false);
         console.error('Error updating job:', error);
@@ -61,42 +59,59 @@ const JobUpdate = () => {
 
   return (
     <div>
-      <h1>Edit Job</h1>
-      {loading ? <div>Loading...</div> : (
-        <div>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+      <div className="flex flex-wrap min-h-screen w-full content-center justify-center bg-gray-200 py-10">
+        <div className="flex shadow-md">
+          <div className="flex flex-wrap content-center justify-center rounded-l-md bg-white" style={{ width: '50rem', height: '100%' }}>
+            <div className="w-72" style={{ width: '45rem' }}>
+              <h1 className="text-3xl font-semibold mb-2">Edit Job</h1>
+              {loading ? (
+                <div>Loading...</div>
+              ) : (
+                <div>
+                  <div className="mb-6">
+                    <label htmlFor="title" className="block text-sm font-semibold mb-2">Title</label>
+                    <input type="text" placeholder="Job Title" value={title} className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-2 px-3 text-lg text-gray-700" id='title' onChange={(e) => { setTitle(e.target.value) }} />
+                  </div>
+                  <div className="mb-6">
+                    <label htmlFor="department" className="block text-sm font-semibold mb-2">Department</label>
+                    <input type="text" placeholder="Department" value={department} className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-2 px-3 text-lg text-gray-700" id='department' onChange={(e) => { setDepartment(e.target.value) }} />
+                  </div>
+                  <div className="mb-6">
+                    <label htmlFor="description" className="block text-sm font-semibold mb-2">Description</label>
+                    <textarea rows="4" placeholder="Job Description" value={description} id='description' className="w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-4 text-base text-gray-700 outline-none focus:border-purple-700 focus:shadow-md" style={{ height: '8rem', overflowY: 'auto' }} onChange={(e) => { setDescription(e.target.value) }}></textarea>
+                  </div>
+                  <div className="mb-6">
+                    <label htmlFor="location" className="block text-sm font-semibold mb-2">Location</label>
+                    <input type="text" placeholder="Job Location" value={location} className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-2 px-3 text-lg text-gray-700" id='location' onChange={(e) => { setLocation(e.target.value) }} />
+                  </div>
+                  <div className="mb-6">
+                    <label htmlFor="salary" className="block text-sm font-semibold mb-2">Salary</label>
+                    <input type="text" placeholder="Salary" value={salary} className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-2 px-3 text-lg text-gray-700" id='salary' onChange={(e) => { setSalary(e.target.value) }} />
+                  </div>
+                  <div className="mb-6">
+                    <label htmlFor="requirements" className="block text-sm font-semibold mb-2">Requirements</label>
+                    <textarea rows="4" placeholder="Job Requirements" value={requirements} id='requirements' className="w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-4 text-base text-gray-700 outline-none focus:border-purple-700 focus:shadow-md" style={{ height: '8rem', overflowY: 'auto' }} onChange={(e) => { setRequirements(e.target.value) }}></textarea>
+                  </div>
+                  <div className="mb-6">
+                    <label htmlFor="postedBy" className="block text-sm font-semibold mb-2">Posted By</label>
+                    <input type="text" placeholder="Posted By" value={postedBy} className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-2 px-3 text-lg text-gray-700" id='postedBy' onChange={(e) => { setPostedBy(e.target.value) }} />
+                  </div>
+                  <div>
+                    <button className="mb-2 block w-full text-center text-white bg-purple-700 hover:bg-purple-900 py-2 px-3 rounded-md" onClick={handleUpdate}>Update</button>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-          <div>
-            <label htmlFor="department">Department:</label>
-            <input id="department" type="text" value={department} onChange={(e) => setDepartment(e.target.value)} />
+          <div className="flex flex-wrap content-center justify-center rounded-r-md" style={{ width: '24rem', height: '100%' }}>
+          <img className="w-full h-full bg-center bg-no-repeat bg-cover rounded-r-md" src={jobImage} alt="Job Banner" />
+            {/* You can add the image here */}
           </div>
-          <div>
-            <label htmlFor="description">Description:</label>
-            <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
-          </div>
-          <div>
-            <label htmlFor="location">Location:</label>
-            <input id="location" type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
-          </div>
-          <div>
-            <label htmlFor="salary">Salary:</label>
-            <input id="salary" type="text" value={salary} onChange={(e) => setSalary(e.target.value)} />
-          </div>
-          <div>
-            <label htmlFor="requirements">Requirements:</label>
-            <textarea id="requirements" value={requirements} onChange={(e) => setRequirements(e.target.value)}></textarea>
-          </div>
-          <div>
-            <label htmlFor="postedBy">Posted By</label>
-            <textarea id="postedBy" value={postedBy} onChange={(e) => setPostedBy(e.target.value)}></textarea>
-          </div>
-          <button className="edit-button" onClick={handleUpdate}>Update</button>
         </div>
-      )}
+      </div>
     </div>
-  );
+  ); 
 };
 
 export default JobUpdate;
--- frontend/src/Admin/components/JobsTable.jsx
+++ frontend/src/Admin/components/JobsTable.jsx
@@ -79,29 +79,23 @@ export default function JobsTable() {
 
     return (
         <div className="text-gray-900 bg-gray-200">
-        <div className="flex flex-col">
-            <div className="p-4">
+            <div className="p-4 flex">
                 <h1 className="text-3xl">Jobs</h1>
             </div>
-            <div className="p-4 ml-4">
-                <button className="top-left-button">Add a Job</button>
+            <div className="px-3 py-4">
+                {/* DataTable component with pagination */}
+                <DataTable
+                    columns={columns}
+                    data={jobs}
+                    pagination
+                    paginationPerPage={10} // Number of rows per page
+                    paginationRowsPerPageOptions={[10, 20, 30]} // Options for rows per page
+                    striped
+                    highlightOnHover
+                />
             </div>
         </div>
-        <div className="px-3 py-4 flex justify-center">
-            <DataTable
-                columns={columns}
-                data={jobs}
-                fixedHeader
-                pagination
-            />
-        </div>
-        <div className="pagination-container">
-            <nav className="sc-iKOmoZ hVPalX rdt_Pagination">
-                {/* Pagination component code goes here */}
-            </nav>
-        </div>
-    </div>
-);
+    );
 }
 
 function DeleteJob({ id, title, handleDelete }) {
