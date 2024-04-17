dda18a543d6ee3424a91b1b89c98df58982c434a
pemindi <pemindiw@gmail.com>
2024-04-17 11:06:05 +0530

    jobs

--- frontend/src/Admin/AdminPackage.jsx
+++ frontend/src/Admin/AdminPackage.jsx
@@ -3,7 +3,7 @@ import './adminStyles/AdminLayout.css'
 import SideBar from './components/SideBar'
 import Header from './components/Header'
 import AllPackages from './components/AllPackages'
-import Package from './pages/'
+import Package from './pages/Package'
 
 
 
