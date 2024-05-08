04e88daf2728802497c97ea574db4e8fe6cd4386
Thisara <sasmithajayasinghe1@gmail.com>
2024-05-08 11:20:24 +0530

    cook

--- backend/models/PackageModel.js
+++ backend/models/PackageModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const packageSchema = new mongoose.Schema({
     packageName:{
         type:String,
-        required: true,
+        // required: true,
     },
     packageId:{
         type:String,
